4152e3af865105ef9d642d6ee0d66f5827a8442c
Austin <dwomohaustin14@gmail.com>
2024-09-17 00:01:49 -0500

    Update slideshow.php

--- includes/slideshow.php
+++ includes/slideshow.php
@@ -7,21 +7,21 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
-      <img src="/assets/images/slideshow/1.1.jpg" class="d-block w-100 image-fluid" alt="Image 1">
+      <img src="assets/images/slideshow/1.1.jpg" class="d-block w-100 image-fluid" alt="Image 1">
       <div class=" blurred-background carousel-caption d-none d-md-block ">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="2000">
-       <img src="/assets/images/slideshow/clubsimg.jpg" class="d-block w-100 image-fluid" alt="Image 2">
+       <img src="assets/images/slideshow/clubsimg.jpg" class="d-block w-100 image-fluid" alt="Image 2">
        <div class=" blurred-background carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="2000">
-      <img src="/assets/images/slideshow/Homeimg.jpg" class="d-block w-100 image-fluid" alt="Image 3">
+      <img src="assets/images/slideshow/Homeimg.jpg" class="d-block w-100 image-fluid" alt="Image 3">
       <div class="blurred-background carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
@@ -29,7 +29,7 @@
     </div>
 
     <div class="carousel-item" data-bs-interval="2000">
-     <img src="/assets/images/slideshow/IMG-20240912-WA0202.jpg" class="d-block w-100 image-fluid" alt="Image 4">
+     <img src="assets/images/slideshow/IMG-20240912-WA0202.jpg" class="d-block w-100 image-fluid" alt="Image 4">
       <div class="blurred-background carousel-caption d-none d-md-block">
         <h5>Fourth slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
@@ -37,7 +37,7 @@
     </div>
 
     <div class="carousel-item" data-bs-interval="2000">
-      <img src="/assets/images/slideshow/IMG-20240912-WA0201.jpg" class="d-block w-100 image-fluid" alt="Image 5">
+      <img src="assets/images/slideshow/IMG-20240912-WA0201.jpg" class="d-block w-100 image-fluid" alt="Image 5">
       <div class="blurred-background carousel-caption d-none d-md-block">
         <h5>Fifth slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
